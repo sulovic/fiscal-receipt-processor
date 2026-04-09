2566401704c03ce80276dcaebea00c56171fe715
Vladimir Sulovic <sulovic@gmail.com>
2026-04-09 08:29:16 +0200

    Removed prisma migrate from dockerfile

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -6,6 +6,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.DATABASE_FISCALRECEIPTS_URL,
+    url: env("DATABASE_FISCALRECEIPTS_URL"),
   },
 });
